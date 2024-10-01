35c4393778ef8bff400e40359fca793fac895349
Peter Sirotnak <sirotnak.p@gmail.com>
2024-10-01 14:01:09 +0200

    Skeleton for running percona package tests on local machine

---
+++ local-package-tests/run_package_test.sh
@@ -0,0 +1,33 @@
+i=1;
+
+for flag in "$@" 
+do
+    if [[ $flag =~ "playbook" ]]
+    then
+        playbook_file=$2
+        shift 2
+    elif [[ $flag =~ "pmm-version" ]]; then
+        pmm_version=$2
+        shift 2
+    fi
+    echo "Playbook file is: $playbook_file"
+    echo "PMM Version is: $pmm_version"
+done
+
+os_distribution=$(cat /etc/*-release | grep "DISTRIB_ID" | awk -F '=' '{print $2}')
+if [[ $os_distribution =~ "Ubuntu" ]]
+then
+  chmod +x install-dependencies-ubuntu.sh
+  ./install-dependencies-ubuntu.sh
+fi
+
+
+export ADMIN_PASSWORD=admin
+export PMM_SERVER_IP=$(sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' pmm-server)
+export PMM_VERSION=$pmm_version
+
+sudo docker compose -f docker-compose-pmm-server-basic.yml up -d
+
+git clone https://github.com/Percona-QA/package-testing.git
+
+ansible-playbook --connection=local --inventory 127.0.0.1, --limit 127.0.0.1 package-testing/playbooks/$playbook_file.yml
