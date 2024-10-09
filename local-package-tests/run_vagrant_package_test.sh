ff90e6dc34ec50848c822fee796693f8ee25206a
Peter Sirotnak <sirotnak.p@gmail.com>
2024-10-09 12:20:44 +0200

    tests for ssh issue

--- local-package-tests/run_vagrant_package_test.sh
+++ local-package-tests/run_vagrant_package_test.sh
@@ -17,12 +17,27 @@ fi
 
 git clone https://github.com/Percona-QA/package-testing.git
 
+
+#if wsl
+#config.vm.provider "virtualbox" do |vb|
+ #
+ #     vb.gui = true
+ #   end
+
 cat > Vagrantfile <<EOF
 Vagrant.require_version ">= 1.7.0"
 Vagrant.configure(2) do |config|
-  config.vm.box="generic/ubuntu2204"
-  config.vm.synced_folder '.', '/vagrant', disabled: true
-  config.ssh.insert_key = false
+    config.vm.provider "virtualbox" do |vb|
+        vb.customize ["modifyvm", :id, "--uart1", "0x3F8", "4"]
+        vb.customize ["modifyvm", :id, "--uartmode1", "file", File::NULL]
+
+        vb.customize ["modifyvm", :id, "--nestedpaging", "off"]
+        vb.customize ["modifyvm", :id, "--cpus", 4]
+        vb.customize ["modifyvm", :id, "--paravirtprovider", "hyperv"]
+    end
+    config.vm.box="generic/ubuntu2204"
+    config.vm.synced_folder '.', '/vagrant', disabled: true
+    config.ssh.insert_key = false
 end
 EOF
 
