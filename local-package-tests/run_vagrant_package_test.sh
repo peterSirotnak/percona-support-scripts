84fdbbd0639bd8e0cc52ed304bbe5cf4c5a57d8c
Peter Sirotnak <sirotnak.p@gmail.com>
2024-10-09 12:04:43 +0200

    Refactors and clones package testing

--- local-package-tests/run_vagrant_package_test.sh
+++ local-package-tests/run_vagrant_package_test.sh
@@ -4,19 +4,25 @@
 export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
 export PATH="$PATH:/mnt/c/Program Files/Oracle/VirtualBox"
 
-PLUGIN_PRESENT=$(vagrant plugin list | grep "vbguest")
+VBGUEST_PLUGIN_PRESENT=$(vagrant plugin list | grep "vbguest")
+SCP_PLUGIN_PRESENT=$(vagrant plugin list | grep "vagrant-scp")
 
-if [ -z "$PLUGIN_PRESENT" ]; then
+if [ -z "$VBGUEST_PLUGIN_PRESENT" ]; then
     vagrant plugin install vagrant-vbguest
 fi
 
+if [ -z "$SCP_PLUGIN_PRESENT" ]; then
+    vagrant plugin install vagrant-scp
+fi
+
+git clone https://github.com/Percona-QA/package-testing.git
+
 cat > Vagrantfile <<EOF
 Vagrant.require_version ">= 1.7.0"
-
 Vagrant.configure(2) do |config|
   config.vm.box="generic/ubuntu2204"
   config.vm.synced_folder '.', '/vagrant', disabled: true
-
+  config.ssh.insert_key = false
 end
 EOF
 
